ef7d26d43085758c7e5fabd550fbef9c37882303
AURELIEN2004 <feudjioaurelien24@gmail.com>
2025-08-06 16:48:13 +0100

    modif

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './home.css'
-// import video from '../../Assets/video.mp4'
+ import video from '../../Assets/video.mp4'
+//  import {Grlocation} from 'react-icons/gr'
 
 const Home = () => {
   return (
     <section className="home">
       <div className="overlay"></div>
-      {/* <video src={video} muted autoPlay-loop-type="video/mp4"></video> */}
+       <video src={video} muted autoPlay-loop-type="video/mp4"></video> 
 
       <div className="homeContent container">
         <div className="textDiv">
@@ -18,6 +19,35 @@ const Home = () => {
           <h1 className="homeTitle">
             search your holiday
           </h1>
+        </div>
+
+        <div className="carDiv grid">
+
+          <div className="destinationInput">
+            <label htmlFor="city">Search your destination</label>
+            <div className="input flex">
+              <input type="text" placeholder="enter name here..." />
+              {/* <Grlocation className="icon"/> */}
+            </div>
+          </div>
+
+           <div className="dateInput">
+            <label htmlFor="date">Select your date</label>
+            <div className="input-flex">
+              <input type="date"/>
+            </div>
+          </div>
+
+           <div className="priceInput">           
+            <div className="label_total flex">
+              <label htmlFor="price">max price:</label>
+               <h3 className="total">fcfa5000</h3>
+            </div>
+            <div className="input flex">
+              <input type="range" max="5000" min="1000"/>
+            </div>
+          </div>
+
         </div>
       </div>
 

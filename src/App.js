c145b9cd5342e315593efbc0ffd7e514d5d2841c
AURELIEN2004 <feudjioaurelien24@gmail.com>
2025-08-06 07:05:39 +0100

    modif

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
-import './app.css';
+import './App.css';
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Components/Home/Home";
+import Main from "./Components/Main/Main";
+import Footer from "./Components/Footer/Footer";
 const App = () => {
   return (
-    <div>
-      <h1>Welcome to the Travel App</h1>
-    </div>
-  );
+    <>
+    <Navbar/>
+    <Home/>
+    <Main/>
+    <Footer/>
+
+    </>
+  )
 }
 export default App;

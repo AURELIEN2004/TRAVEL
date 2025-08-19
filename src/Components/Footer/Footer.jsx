deb8d26f97be4a776664302d6a8b3d32fc290eac
AURELIEN2004 <feudjioaurelien24@gmail.com>
2025-08-19 10:11:15 +0100

    modif

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,150 @@
 import React from 'react'
-import './footer.css'
+import './footer.scss'
+import video2 from'../../Assets/video2.mp4';
+import { FiChevronRight, FiSend } from 'react-icons/fi';
+import {MdOutlineTravelExplore} from'react-icons/md'
+import { AiFillInstagram, AiFillYoutube, AiOutlineTwitter } from 'react-icons/ai';
+import { FaTripadvisor } from 'react-icons/fa';
+
 
 const Footer = () => {
   return (
-    <div>
-      footer
-    </div>
+    <section className="footer">
+      <div className="videoDiv">
+        <video src={video2} loop autoPlay muted type="video/mp4"></video>
+      </div>
+
+      <div className="secContent container">
+        <div className="contactDiv flex">
+          <div className="text">
+               <small>KEEP IN TOUCH</small>
+               <h2>Travel with us</h2>
+          </div>
+
+          <div className="inputDiv flex">
+            <input type="text" placeholder='Enter Email Address' />
+            <button className='btn flex' type='submit'>
+              SEND<FiSend className='icon'/>
+            </button>
+          </div>
+        </div>
+
+        <div className="footerCard ">
+            <div className="footerIntro flex">
+                <div className="logoDiv">
+                   <a href=""className='logo flex'>
+                      <MdOutlineTravelExplore className='icon'/>
+                      Travel.
+                   </a>
+                </div>
+
+                <div className="footerParagraph">
+                  loren ipsun dolor sit amet, concectetur adipisicing elit                   loren ipsun dolor sit amet, concectetur adipisicing elit
+
+                </div>
+                <div className="footerSocials">
+                    <AiOutlineTwitter className='icon'/>
+                    <AiFillYoutube className='icon'/>
+                    <AiFillInstagram className='icon'/>
+                    <FaTripadvisor className='icon'/>
+
+                </div>
+            </div>
+
+            <div className="footerLinks grid">
+                <div className="linkGroup">
+                   <span className="groupTitle">
+                      OUR AGENCY
+                   </span>
+
+                   <li className="footerList flex">
+                      <FiChevronRight className='icon'/>
+                      Services
+                   </li>
+                   <li className="footerList flex">
+                      <FiChevronRight className='icon'/>
+                      Insurance
+                   </li>
+                   <li className="footerList flex">
+                      <FiChevronRight className='icon'/>
+                      Agency
+                   </li>
+                   <li className="footerList flex">
+                      <FiChevronRight className='icon'/>
+                      Tourism
+                   </li> 
+                   <li className="footerList flex">
+                      <FiChevronRight className='icon'/>
+                      Payment
+                   </li>                                                                        
+                </div>
+
+                <div className="linkGroup">
+                   <span className="groupTitle">
+                      PARTNERS
+                   </span>
+
+                   <li className="footerList flex">
+                      <FiChevronRight className='icon'/>
+                      Services
+                   </li>
+                   <li className="footerList flex">
+                      <FiChevronRight className='icon'/>
+                      Insurance
+                   </li>
+                   <li className="footerList flex">
+                      <FiChevronRight className='icon'/>
+                      Agency
+                   </li>
+                   <li className="footerList flex">
+                      <FiChevronRight className='icon'/>
+                      Tourism
+                   </li> 
+                   <li className="footerList flex">
+                      <FiChevronRight className='icon'/>
+                      Payment
+                   </li>                                                                        
+                </div> 
+
+                <div className="linkGroup">
+                   <span className="groupTitle">
+                      LAST MINUTE
+                   </span>
+
+                   <li className="footerList flex">
+                      <FiChevronRight className='icon'/>
+                      Services
+                   </li>
+                   <li className="footerList flex">
+                      <FiChevronRight className='icon'/>
+                      Insurance
+                   </li>
+                   <li className="footerList flex">
+                      <FiChevronRight className='icon'/>
+                      Agency
+                   </li>
+                   <li className="footerList flex">
+                      <FiChevronRight className='icon'/>
+                      Tourism
+                   </li> 
+                   <li className="footerList flex">
+                      <FiChevronRight className='icon'/>
+                      Payment
+                   </li>                                                                        
+                </div>  
+
+            </div>
+
+            <div className="footerDiv flex">
+              <small>BEST TRAVEL WEBSITE THEME</small>
+              <small>COPYRIGHTS RESERVED . IRATECH 2002</small>
+
+            </div>
+        </div>
+
+      </div>
+
+    </section>
   )
 }
 
--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -9,130 +9,109 @@ import img3 from '../../Assets/img3.jpg'
 import img4 from '../../Assets/img4.jpg'
 import img5 from '../../Assets/img5.jpeg'
 import img6 from '../../Assets/img6.jpg'
-// import img1 from '../../Assets/img7.jpg'
-// import img1 from '../../Assets/img8.jpg'
-// import img1 from '../../Assets/img9.jpeg'
-
 
 const Data = [
   {
-    id:1,
+    id: 1,
     imgSrc: img1,
-    destTitle: "bora" ,
-    location:"new zealand",
-    grade: "cuttural relax",
+    destTitle: "Bora Bora",
+    location: "New Zealand",
+    grade: "Cultural Relax",
     fees: "$788",
-    description:"tres beau logement situer en bordure de mer situertres beau logement situer en bordure de mer situer",
-
+    description: "Très beau logement situé en bordure de mer avec une vue imprenable sur l'océan.",
   },
-    {
-    id:2,
+  {
+    id: 2,
     imgSrc: img2,
-    destTitle: "bora alca" ,
-    location:"new zealand",
-    grade: "cuttural relax",
+    destTitle: "Bora Alca",
+    location: "New Zealand",
+    grade: "Cultural Relax",
     fees: "$788",
-    description:"tres beau logement situer en bordure de mer",
-
-  }
-  ,
-    {
-    id:3,
+    description: "Très beau logement situé en bordure de mer avec des activités nautiques.",
+  },
+  {
+    id: 3,
     imgSrc: img3,
-    destTitle: "bora" ,
-    location:"new zealand",
-    grade: "cuttural relax",
+    destTitle: "Paradise Bay",
+    location: "New Zealand",
+    grade: "Cultural Relax",
     fees: "$788",
-    description:"tres beau logement situer en bordure de mer",
-
-  }
-  ,
-    {
-    id:4,
+    description: "Très beau logement situé en bordure de mer dans un cadre idyllique.",
+  },
+  {
+    id: 4,
     imgSrc: img4,
-    destTitle: "bora" ,
-    location:"new zealand",
-    grade: "cuttural relax",
+    destTitle: "Ocean View",
+    location: "New Zealand",
+    grade: "Cultural Relax",
     fees: "$788",
-    description:"tres beau logement situer en bordure de mer",
-
-  }
-  ,
-    {
-    id:5,
+    description: "Très beau logement situé en bordure de mer avec terrasse privée.",
+  },
+  {
+    id: 5,
     imgSrc: img5,
-    destTitle: "bora" ,
-    location:"new zealand",
-    grade: "cuttural relax",
+    destTitle: "Sunset Beach",
+    location: "New Zealand",
+    grade: "Cultural Relax",
     fees: "$788",
-    description:"tres beau logement situer en bordure de mer",
-
-  }
-  ,
-    {
-    id:6,
+    description: "Très beau logement situé en bordure de mer pour des couchers de soleil magiques.",
+  },
+  {
+    id: 6,
     imgSrc: img6,
-    destTitle: "bora" ,
-    location:"new zealand",
-    grade: "cuttural relax",
+    destTitle: "Crystal Waters",
+    location: "New Zealand",
+    grade: "Cultural Relax",
     fees: "$788",
-    description:"tres beau logement situer en bordure de mer",
-
+    description: "Très beau logement situé en bordure de mer aux eaux cristallines.",
   },
 ]
 
 const Main = () => {
   return (
     <section className="main container section">
-         
-         <div className="secTitle">
-             <h3 className='title'>
-              Most visited destinations
-              </h3>
-         </div>
+      <div className="secTitle">
+        <h3 className='title'>
+          Most visited destinations
+        </h3>
+      </div>
 
-         <div className="secContent grid">
+      <div className="secContent grid">
+        {Data.map(({ id, imgSrc, destTitle, location, grade, fees, description }) => {
+          return (
+            <div key={id} className="singleDestination">
+              <div className="imageDiv">
+                <img src={imgSrc} alt={destTitle} />
+              </div>
 
-            { Data.map(({id,imgSrc,destTitle,location,grade,fees,description})=>{
-              return(
-                  <div key={id} className="singleDestination">
+              <div className="cardInfo">
+                <h4 className="destTitle">{destTitle}</h4>
+                <span className="continent flex">
+                  <HiOutlineLocationMarker className='icon' />
+                  <span className="name">{location}</span>
+                </span>
 
-                     <div className="imageDiv">
-                        <img src={imgSrc} alt={destTitle} />
-                     </div>
-
-                     <div className="cardInfo">
-                       <h4 className="destTitle">{destTitle}</h4>
-                       <span className="continent flex">
-                         <HiOutlineLocationMarker className='icon'/>
-                         <span className="name">{location}</span>
-                       </span>
-
-                       <div className="fees flex">
-                          <div className="grade">
-                             <span>{grade}<snall>+1</snall></span>
-                          </div>
-                          <div className="price">
-                            <h5>  { fees}</h5>
-                          </div>
-                       </div>
-
-                       <div className="desc">
-                        {description}
-                       </div>
-
-                       <button className='btn flex'>
-                        DETAILS <HiOutlineClipboardCheck className='icon'/>
-                       </button>
-                     </div>
+                <div className="fees flex">
+                  <div className="grade">
+                    <span>{grade}<small>+1</small></span>
                   </div>
-                     
+                  <div className="price">
+                    <h5>{fees}</h5>
+                  </div>
+                </div>
 
-              )
-             })}
+                <div className="desc">
+                  {description}
+                </div>
 
-         </div>
-
+                <button className='btn flex'>
+                  DETAILS <HiOutlineClipboardCheck className='icon' />
+                </button>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </section>
   )
 }
